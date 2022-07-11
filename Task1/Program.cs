138d9f19e6717425fb013b444e03eaf19fece47d
Anisa <An1sa_jon@mail.ru>
2022-07-11 22:35:40 +0400

    added Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Enter a three-digit number");
+int = Convert.ToInt32(Console.ReadLine());
+
+void FindSecondDigit (int arg)
+ {
+    if (arg >= 100) 
+    {
+        result = (num % 100) / 10;
+        Console.WriteLine(result);
+    }
+    else {
+        Console.WriteLine("number is not three digits");
+    }
+}
+
+FindSecondDigit(num); 
